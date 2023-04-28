71f5ff4049532083a355de95266545354f146335
kunjk <kunjkoradiya09@gmail.com>
2023-04-28 23:11:21 +0530

    yuy2

--- app/src/main/java/com/example/suburban/FSeller_mobile_verification.java
+++ app/src/main/java/com/example/suburban/FSeller_mobile_verification.java
@@ -1,42 +1,14 @@
 package com.example.suburban;
 
-import static android.view.View.VISIBLE;
-
-import android.app.ActionBar;
-import android.content.Intent;
 import android.os.Bundle;
-import com.google.firebase.FirebaseApp;
-import androidx.activity.OnBackPressedCallback;
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.AppCompatButton;
-import androidx.appcompat.widget.Toolbar;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.EditText;
-import android.widget.ImageView;
-import android.widget.ProgressBar;
-import android.widget.TextView;
-import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.FirebaseException;
-import com.google.firebase.FirebaseTooManyRequestsException;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.auth.PhoneAuthCredential;
-import com.google.firebase.auth.PhoneAuthOptions;
-import com.google.firebase.auth.PhoneAuthProvider;
-
-import java.util.concurrent.TimeUnit;
+import androidx.appcompat.widget.AppCompatButton;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 public class FSeller_mobile_verification extends Fragment {
 
@@ -50,14 +22,7 @@ public class FSeller_mobile_verification extends Fragment {
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_f_seller_mobile_verification, container, false);
 
-        AppCompatButton Seller_login = view.findViewById(R.id.Seller_Login);
-
-
-
-
-
-
-
+        AppCompatButton Seller_login = view.findViewById(R.id.login_button);
 
         Seller_login.setOnClickListener(new View.OnClickListener() {
             @Override
---
+++ app/src/main/java/com/example/suburban/Registration_seller.java
@@ -0,0 +1,25 @@
+package com.example.suburban;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+
+public class Registration_seller extends Fragment {
+
+    public Registration_seller() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+
+
+        return inflater.inflate(R.layout.fragment_registration_seller, container, false);
+    }
+}
